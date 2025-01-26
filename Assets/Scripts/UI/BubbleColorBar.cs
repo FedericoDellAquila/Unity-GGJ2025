6be81ff40b44ad100d8a03b40ad65a9633789397
JJJJJJack <97jack@live.it>
2025-01-26 03:42:32 +0100

    ADD: `BubbleColorBar.cs` script, added its own prefab, put in the `UI > Canvas` GameObject, still need to test with proper microphone and di any gain adjustment

---
+++ Assets/Scripts/UI/BubbleColorBar.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleColorBar : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private MicrophoneLoudnessDetector microphoneLoudnessDetector = null;
+
+    private List<Slider> quartersSliders = new List<Slider>();
+
+    void Start()
+    {
+        microphoneLoudnessDetector = player.GetComponent<MicrophoneLoudnessDetector>();
+
+        foreach (Transform child in transform)
+        {
+            Slider slider = child.GetComponent<Slider>();
+            if (slider != null)
+            {
+                quartersSliders.Add(slider);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (microphoneLoudnessDetector == null)
+        {
+            // Debug.LogError("MicrophoneLoudnessDetector is null");
+            return;
+        }
+        float value = microphoneLoudnessDetector.loudness;
+
+        if (value > 1f)
+        {
+            value = 1f;
+        }
+
+        value *= 4f;
+        foreach (Slider slider in quartersSliders)
+        {
+            if (value > 1f)
+            {
+                slider.value = 1f;
+                value -= 1f;
+                continue;
+            }
+            slider.value = value;
+            value = 0f;
+        }
+    }
+}
